e83f77b322b558736abe6367b7a31b83a2c7ed86
DAGARAL <dagaral1@gmail.com>
2015-10-22 18:53:12 -0400

    Update
    Completion of the program

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverted <- NULL ## initialize inverted as void
+  set <- function(y) {
+    x <<- y  ## sends the value to the global environment (outside the function)
+    inv <<- NULL ## sends the value to the global environment (outside the function)
+  }
+  get <- function() x
+  setinverse <- function(inverse) inverted <<- inverse
+  getinverse <- function() inverted
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverted <- x$getinverse()
+  if(!is.null(inverted)) { # checks if the inverted already existis
+    message("getting cached data.")
+    return(inverted) # if it exists, returns the inverted matrix that is store
+  }
+ 
+  inverted <- solve(x$get()) # if it doesn't exists, then it calculates the inverse with solve()
+  x$setinverse(inverted) # we set the inverse in the object X so next time the function knows that it is already done
+  inverted
 }
